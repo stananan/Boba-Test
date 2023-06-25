ae050dd1521a124820eea9950417688674e57165
stananan <stanleyho862@gmail.com>
2023-06-24 23:19:24 -0700

    CSS changes

--- aic/stresstest/action.php
+++ aic/stresstest/action.php
@@ -17,12 +17,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boba test</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="test.css">
     <script src="main.js"></script>
 </head>
 <body>
+    <div class='header'><h1>Welcome to my Boba Test</h1><h2>Created by Stanley Ho</h2></div>
+
     <?php
         require_once("header.php");
-        printHeader();
+        //printHeader();
         //var_dump($_POST);
         foreach($_POST as $key => $val){
             if(is_array($val)){
